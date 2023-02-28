54860ee077ed690744d8c6d51c03375daebe6bb8
gerel <gerel.budaeva@gmail.com>
2023-02-28 11:04:50 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [tank, setTank] = useState(0);
+    const [mpg, setMpg] = useState(0);
+    const [distance, setDistance] = useState(0);
+    const [result, setResult] = useState('');
+
+    const onTankChange = (e) => {
+        setTank(e.target.value);
+        calculate()
+    }
+
+    const onMpgChange = (e) => {
+        setMpg(e.target.value);
+        calculate()
+    }
+
+    const onDistanceChange = (e) => {
+        setDistance(e.target.value);
+        calculate()
+    }
+
+    const calculate = () => {
+        const carDistance = tank * mpg;
+        if (carDistance >= distance) setResult('We arrived!')
+        else setResult('We did not arrive!')
+    }
+
+    return (
+        <div className="App">
+
+            <div>
+                <label for="tank">Tank, Gal</label>
+                <input value={tank} onChange={onTankChange} name="tank" type="number"/>
+            </div>
+            <div>
+                <label htmlFor="mpg">Mpg</label>
+                <input value={mpg} onChange={onMpgChange} name="mpg" type="number"/>
+            </div>
+            <div>
+                <label htmlFor="distance">Distance, Miles</label>
+                <input value={distance} onChange={onDistanceChange} name="distance" type="number"/>
+            </div>
+
+            <hr/>
+
+            <div>{tank}</div>
+            <div>{mpg}</div>
+            <div>{distance}</div>
+
+            <hr/>
+            {result}
+        </div>
+    );
 }
 
 export default App;
